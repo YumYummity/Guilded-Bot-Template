46f1d49a294aa47c532e68bb7ddaa18d8fe04a18
YumYummity <103061664+YumYummity@users.noreply.github.com>
2024-01-11 20:15:18 -0500

    Status changing lgos

--- COGS/tasks.py
+++ COGS/tasks.py
@@ -14,15 +14,16 @@ class tasks(commands.Cog):
             server_count = len(await self.bot.fetch_servers())
             statuses = [
                 [None, f"Watching {server_count} server{'s' if server_count != 1 else ''}!", 10],
-                [None, f"Helping you build Guilded bots!", 10],
-                [None, f"https://github.com/YumYummity/Guilded-Bot-Template/tree/main", 10]
+                [None, f"Helping you build Guilded bots!", 30],
+                [None, f"https://github.com/YumYummity/Guilded-Bot-Template/tree/main", 60]
             ]
             for status in statuses:
                 try:
                     await self.bot.set_status(status[0] if status[0] else 90002547, content=status[1])
+                    self.bot.info(f"Status changed to {self.bot.COLORS.item_name}{status[1]}{self.bot.COLORS.normal_message} with emoji id {self.bot.COLORS.item_name}{status[0] if status[0] else '90002547 (None)'}")
                     await asyncio.sleep(status[2])
-                except:
-                    pass
+                except Exception as e:
+                    self.bot.warn(f"An error occurred while attempting to change the bot's status: {self.bot.COLORS.item_name}{e}")
 
 def setup(bot):
 	bot.add_cog(tasks(bot))
