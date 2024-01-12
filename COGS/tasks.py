e50b1918526416e93f6ee41de30c7b59e181f4f7
YumYummity <103061664+YumYummity@users.noreply.github.com>
2024-01-11 21:07:50 -0500

    More status logging

--- COGS/tasks.py
+++ COGS/tasks.py
@@ -20,7 +20,7 @@ class tasks(commands.Cog):
             for status in statuses:
                 try:
                     await self.bot.set_status(status[0] if status[0] else 90002547, content=status[1])
-                    self.bot.info(f"Status changed to {self.bot.COLORS.item_name}{status[1]}{self.bot.COLORS.normal_message} with emoji id {self.bot.COLORS.item_name}{status[0] if status[0] else '90002547 (None)'}")
+                    self.bot.info(f"Status changed to {self.bot.COLORS.item_name}{status[1]}{self.bot.COLORS.normal_message} with emoji id {self.bot.COLORS.item_name}{status[0] if status[0] else '90002547 (None)'}{self.bot.COLORS.normal_message} for {self.bot.COLORS.item_name}{status[2]}{self.bot.COLORS.normal_message} seconds")
                     await asyncio.sleep(status[2])
                 except Exception as e:
                     self.bot.warn(f"An error occurred while attempting to change the bot's status: {self.bot.COLORS.item_name}{e}")
